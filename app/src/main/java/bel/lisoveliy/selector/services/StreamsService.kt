d9daa816dad093111d75a15d022ecbea8d357f81
Gleb <tomakhgleb2003@gmail.com>
2023-01-21 11:52:43 +0300

    StreamCreateVM update

--- app/src/main/java/bel/lisoveliy/selector/services/StreamsService.kt
+++ app/src/main/java/bel/lisoveliy/selector/services/StreamsService.kt
@@ -1,11 +1,27 @@
 package bel.lisoveliy.selector.services
 
+import bel.lisoveliy.selector.models.Stream
 import bel.lisoveliy.selector.repos.Repo
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class StreamsService(
     private val repo: Repo
 ) {
-    suspend fun check() {
+    private val _streams = MutableStateFlow<List<Stream>>(listOf())
+    val streams = _streams.asStateFlow()
 
+    suspend fun addStream(stream: Stream): AddStreamResponse {
+        //Checks
+        _streams.value = _streams.value + stream
+        return  AddStreamResponse.OK
     }
+
+    suspend fun check() {}
+}
+
+enum class AddStreamResponse {
+    OK,
+    STREAM_WITH_TITLE_ALREADY_EXISTS,
+
 }
--- app/src/main/java/bel/lisoveliy/selector/viewmodels/StreamCreateVM.kt
+++ app/src/main/java/bel/lisoveliy/selector/viewmodels/StreamCreateVM.kt
@@ -1,19 +1,62 @@
 package bel.lisoveliy.selector.viewmodels
 
-import androidx.compose.runtime.compositionLocalOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import bel.lisoveliy.selector.models.Stream
+import bel.lisoveliy.selector.models.StreamType
+import bel.lisoveliy.selector.services.AddStreamResponse
 import bel.lisoveliy.selector.services.StreamsService
+import kotlinx.coroutines.launch
 
 class StreamCreateVM(private val service: StreamsService): ViewModel() {
     var url by mutableStateOf("")
+    var urlError by mutableStateOf<String?>(null)
         private set
+
     var title by mutableStateOf("")
+    var titleError by mutableStateOf<String?>(null)
+
+    var type by mutableStateOf(StreamType.M3U8)
+
+    var creating by mutableStateOf(false)
         private set
+
+    fun create() {
+        if (creating) return
+        viewModelScope.launch {
+            creating = true
+
+            val stream = Stream(
+                title = title,
+                url = url,
+                type = type)
+
+            when(service.addStream(stream)) {
+                AddStreamResponse.OK -> clear()
+                AddStreamResponse.STREAM_WITH_TITLE_ALREADY_EXISTS -> {
+                    TODO()
+                }
+            }
+
+            creating = false
+        }
+    }
+
+    fun clear() {
+        url = ""
+        urlError = null
+
+        title = ""
+        titleError = null
+
+        type = StreamType.M3U8
+    }
 
     companion object {
         val LocalProvider = compositionLocalOf<StreamCreateVM> { error("not found StreamCreateVM") }
+        val local: StreamCreateVM
+            @Composable
+            get() = LocalProvider.current
     }
 }
